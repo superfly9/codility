9ade7ea3d62dc02ae31bf49ceee87573adfdd958
송승찬 <superfly09@daum.net>
2024-09-24 23:39:37 +0900

    feat: easy/prefix-sum

---
+++ leetcode75/prefix-sum/724-find-pivot-index.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var pivotIndex = function (nums) {
+  let total = nums.reduce((a, b) => a + b, 0);
+  let leftTotal = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    let rightTotal = total - leftTotal - nums[i];
+
+    if (rightTotal === leftTotal) {
+      return i;
+    }
+
+    leftTotal += nums[i];
+  }
+
+  return -1;
+};
